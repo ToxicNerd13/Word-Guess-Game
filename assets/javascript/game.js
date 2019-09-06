fbd767bca8b6075d2bcee9ab0fb1fb2931a15140
ToxicNerd <k.lehenbauer13@gmail.com>
2019-09-05 23:54:23 -0700

    Fixed the code as much as I could!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,56 @@
-console.log(this)
-// Create variable for Wins (doesn't reset when hangmanLoop resets bc it's outside?)
-let wins = 0;
-//Create function that runs game when key is pressed
-document.onkeyup = function hangmanLoop() {
+$(document).ready(function () {
     // Create array of words to guess
     let words = [
         "ghost", "witch", "pumpkin", "skeleton", "cauldron"
     ];
-    // Choose random word from array
-    let word = words[Math.floor(Math.random() * words.length)];
-    // Create array for Answer
-    let answer = [];
-    for (let i = 0; i < word.length; i++) {
-        answer[i] = "_";
-    };
     // Create variable for Remaining Guesses
     let remaining = 12;
+    // Create variable for Wins (doesn't reset when hangmanLoop resets bc it's outside?)
+    let wins = 0;
     // Create array for Letters Guessed
     let guessed = [];
+    // Create empty array for Answer (will populate with results of For Loop later)
+    let answer = [];
     // Capture whatever key user pressed 
     let guess = event.key;
-    // Game only runs while Remaining Guesses is more than 0, otherwise it starts the hangmanLoop function over
-    if (remaining === 0) {
-        alert("YOU LOSE! Refresh the page to start over.");
+    //Create function that runs game when key is pressed
+    function newGame() {
+        // Reset Remaining Guesses to 12
+        remaining = 12;
+        //Reset guessed array
+        guessed = [];
+        // Reset answer array
+        answer = [];
+        // Reset Wins
+        wins = 0;
+        // Choose random word from array
+        let word = words[Math.floor(Math.random() * words.length)];
+        for (let i = 0; i < word.length; i++) {
+            answer[i] = "_";
+        };
     }
-    // Scan answer for that letter
-    // if letter is in the word, insert letter in correct spot (index) of answer
-    // else, put letter in "Letters Guessed" (add to end w/append?) and update Guesses Remaining counter
-    for (let x = 0; x < word.length; x++) {
-        if (word[x] === guess) {
-            answer[x] = guess;
+    // Start game loop when key is pressed
+    document.onkeydown = function (event) {
+        // Game only runs while Remaining Guesses is more than 0, otherwise it starts the hangmanLoop function over
+        if (remaining === 0) {
+            alert("YOU LOSE!");
+            newGame();
         }
-        else {
-            remaining--;
-            guessed.push(guess);
+        // Scan answer for the letter pressed
+        // if letter is in the word, insert letter in correct spot (index) of answer
+        // else, put letter in "Letters Guessed" (add to end w/append?) and update Guesses Remaining counter
+        for (let x = 0; x < word.length; x++) {
+            if (word[x] === guess) {
+                answer[x] = guess;
+            }
+            else {
+                remaining--;
+                guessed.push(guess);
+            }
         }
     }
-}
 
-// Add 1 to "Wins" when full word is guessed 
-// Change image and heading over columns to represent the word they just guessed
-// Also clear the old word and supply new ________ 
+    // Add 1 to "Wins" when full word is guessed 
+    // Change image and heading over columns to represent the word they just guessed
+    // Also clear the old word and supply new ________ 
+}
